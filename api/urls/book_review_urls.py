5f0087fcbcd292399382007de46fd00dc53487e7
Hsin Yi Lin <10656025@ntub.edu.tw>
2022-05-05 19:11:04 +0800

    add: reviews-api

--- api/urls/book_review_urls.py
+++ api/urls/book_review_urls.py
@@ -6,7 +6,7 @@ from api.views.book_review_views import *
 app_name = 'book_review'
 
 urlpatterns = [
-    # path('all/', get_all_reviews),
-    # path('get/<int:pk>/', get_review),
-    # path('get_critic_reviews/', get_critic_reviews),
+    path('all/', get_all_reviews),
+    path('get/<int:pk>/', get_review),
+    path('get_critic_reviews/', get_critic_reviews),
 ]
--- api/views/book_review_views.py
+++ api/views/book_review_views.py
@@ -5,13 +5,62 @@ from rest_framework.response import Response
 from api.models import Book
 
 
-# @api_view()
-# def get_all_reviews(request):
+@api_view()
+def get_all_reviews(request):
+    books = Book.objects.all()
+
+    return Response({
+        'success': True,
+        'data': [
+            {
+                'id': book.no,
+                'user_id': book.user.pk,
+                'name': book.name,
+                'title': book.title,
+                'comment': book.comment
+            }
+            for book in books
+        ]
+    })
 
 
-# @api_view()
-# def get_review(request, pk):
+@api_view()
+def get_review(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except:
+        return Response({'success': False, 'message': '查無資料'}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({
+        'success': True,
+        'data': {
+            'id': book.no,
+            'user_id': book.user.pk,
+            'name': book.name,
+            'title': book.title,
+            'comment': book.comment
+        }
+    })
 
 
-# @api_view()
-# def get_critic_reviews(request):
+@api_view()
+def get_critic_reviews(request):
+    data = request.query_params
+    user_id = data.get('user_id')
+
+    user_id = str(user_id).strip()
+    books = Book.objects.filter(user_id=user_id)
+
+    return Response({
+        'success': True,
+        'data': [
+            {
+                'id': book.no,
+                'user_id': book.user.pk,
+                'name': book.name,
+                'title': book.title,
+                'comment': book.comment
+            }
+            for book in books
+        ]
+    })
